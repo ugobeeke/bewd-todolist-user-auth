1f5f76db4c7a44285a45c0f7796ca8c6ff6c89e8
root <root@UGOCHUKWU>
2024-04-21 00:54:12 +0100

    user-create db update

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
-        has_many :tasks, dependent: :destroy  # 
+        has_many :tasks, dependent: :destroy  
+        has_many :sessions
       
     validates :password, presence: true, length: { minimum: 8, maximum: 64 }
 end
