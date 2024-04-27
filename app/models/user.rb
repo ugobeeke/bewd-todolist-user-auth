6ef95671e3083935028f9867cad702cf984be35d
root <root@UGOCHUKWU>
2024-04-27 01:06:03 +0100

    last test

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
         has_many :tasks, dependent: :destroy  
         has_many :sessions
       
-    validates :password, presence: true, length: { minimum: 8, maximum: 64 }
+    validates :username, presence: true, length: { minimum: 3, maximum: 64 }validates :password, presence: true, length: { minimum: 8, maximum: 64 }, uniqueness: true
 
     before_create :hash_password
 
